b761100285a3371a0c91bece5531ddc5621a52fb
zangwoosung <54176171+zangwoosung@users.noreply.github.com>
2025-04-25 00:29:03 +0900

    [fix] spline added

---
+++ Assets/Scripts/Util/SplineChange.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Splines;
+
+public class SplineChange : MonoBehaviour
+{
+    public SplineAnimate anim;
+    public SplineContainer splineA;
+    public SplineContainer splineB;
+    public GameObject newSplineGameObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            anim.Container = splineA;
+            anim.Restart(true);  
+
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            anim.Container = splineB;
+            anim.Restart(true);
+        }
+    }
+}
